2ffef64bec2e0fcdb67aef0303d63cbf21bbb526
azmeer <m.azmeer2003@gmail.com>
2022-09-20 14:12:14 +0500

    U

---
+++ SQL_EASY.sql
@@ -0,0 +1,15 @@
+--https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem?isFullScreen=true
+
+SELECT COUNT(ID) 
+FROM CITY
+WHERE Population > 100000;
+
+--https://www.hackerrank.com/challenges/weather-observation-station-9/problem?isFullScreen=true
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY NOT LIKE 'a%' 
+    AND CITY NOT LIKE 'e%' 
+    AND CITY NOT LIKE 'o%' 
+    AND CITY NOT LIKE 'u%' 
+    AND CITY NOT LIKE 'i%';
