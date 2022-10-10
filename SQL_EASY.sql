b18dbb527cfbb0b4e49f40a8f0ce1a966bc0d02d
azmeer <m.azmeer2003@gmail.com>
2022-10-10 22:47:04 +0500

    3rd

--- SQL_EASY.sql
+++ SQL_EASY.sql
@@ -13,3 +13,16 @@ WHERE CITY NOT LIKE 'a%'
     AND CITY NOT LIKE 'o%' 
     AND CITY NOT LIKE 'u%' 
     AND CITY NOT LIKE 'i%';
+
+    
+
+https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?isFullScreen=true
+
+Select name 
+from city 
+where population > 120000 
+    and
+ Countrycode = "USA";
+
+ 
+
